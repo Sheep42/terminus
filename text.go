ca21204456b0f3461d0eacb378c68e1738c9aef3
Dan Shedd <dan@dshedd.com>
2020-06-16 22:58:40 -0400

    Implement Text

--- text.go
+++ text.go
@@ -1,13 +1,39 @@
 package terminus
 
-// import(
-// 	"github.com/gdamore/tcell"
-// )
-
 type Text struct {
 
-	x int
-	y int
+	*Entity
 	text string
 
+}
+
+func NewText( x, y int, text string ) *Text {
+
+	t := &Text{
+		Entity: NewEntity(x, y),
+		text: text,
+	}
+
+	return t
+
+}
+
+func (text *Text) Init() {
+	text.Entity.Init() // super
+}
+
+func (text *Text) Update() { 
+	text.Entity.Update() // super
+}
+
+func (text *Text) Draw() {
+
+	// override
+	screen := text.Entity.game.screen
+	style := text.Entity.scene.style
+
+	for index, char := range text.text {
+		screen.SetContent(text.x + index, text.y, rune(char), nil, style)
+	}
+	
 }
