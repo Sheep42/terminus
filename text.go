912b12a1f34ac4a1943e795ada58217415f6e651
Dan Shedd <dan@dshedd.com>
2020-08-16 00:21:23 -0400

    Document exported Text functions

--- text.go
+++ text.go
@@ -1,10 +1,14 @@
 package terminus
 
+// Text is a type of Entity which is used
+// to render text to the game screen
 type Text struct {
 	*Entity
 	text string
 }
 
+// NewText takes an x position, y position, and text
+// value and creates a new Text Entity on the screen
 func NewText(x, y int, text string) *Text {
 
 	t := &Text{
@@ -16,14 +20,18 @@ func NewText(x, y int, text string) *Text {
 
 }
 
+// Init fires during game.Init and can be overridden
 func (text *Text) Init() {
 	text.Entity.Init() // super
 }
 
+// Update fires after the scene update on each pass
+// through the game loop, and can be overridden
 func (text *Text) Update(delta float64) {
 	text.Entity.Update(delta) // super
 }
 
+// Draw fires during scene.Draw and can be overridden
 func (text *Text) Draw() {
 
 	// override Entity.Draw
@@ -36,10 +44,12 @@ func (text *Text) Draw() {
 
 }
 
+// SetText sets the text value of the Text Entity
 func (text *Text) SetText(newText string) {
 	text.text = newText
 }
 
+// GetText gets the text value of the Text Entity
 func (text *Text) GetText() string {
 	return text.text
 }
