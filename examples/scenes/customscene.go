9dc5ebe8cd8d5d980b74ad08f44cb72bf8246970
Dan Shedd <dan@dshedd.com>
2020-12-05 21:32:52 -0500

    Add Custom Scene example

---
+++ examples/scenes/customscene.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	t "terminus"
+
+	"github.com/gdamore/tcell"
+	// "strconv"
+	// "github.com/gdamore/tcell"
+)
+
+type CustomScene struct {
+	*t.Scene
+	title *t.Text
+}
+
+func NewCustomScene(g *t.Game, fg, bg tcell.Color, title string) *CustomScene {
+
+	cs := &CustomScene{
+		// NewSceneCustom is like NewScene, but allows
+		// custom foreground and background colors
+		t.NewSceneCustom(g, fg, bg),
+		t.NewText(0, 0, title),
+	}
+
+	return cs
+
+}
+
+func (cs *CustomScene) Setup() {
+
+	cs.Scene.Init() // super
+
+	cs.Add(cs.title)
+
+}
+
+func (cs *CustomScene) Update(delta float64) {
+
+	// Use z and x to cycle through scenes
+	game := cs.Game()
+	input := game.Input()
+
+	// Center the scene title
+	if 0 == cs.title.GetX() {
+
+		screenWidth, screenHeight := game.ScreenSize()
+		textWidth, textHeight := cs.title.GetDimensions()
+
+		cs.title.SetPosition(screenWidth/2-textWidth/2, screenHeight/2-textHeight/2)
+
+	}
+
+	if nil != input {
+
+		if 'z' == input.Rune() {
+
+			game.PrevScene()
+
+		} else if 'x' == input.Rune() {
+
+			game.NextScene()
+
+		}
+
+	}
+
+}
---
+++ examples/scenes/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	t "terminus"
+)
+
+func main() {
+
+	// Create the Game
+	g := t.NewGame()
+
+	// shared text between both scenes, hard-coded color
+	quit := t.NewText(2, 2, "Press ESC to quit", t.White, t.Black)
+
+	// Create the Scenes
+	s := NewCustomScene(g, t.White, t.Blue, "Scene 1 - Press 'x' to see Scene 2")
+	s2 := NewCustomScene(g, t.Black, t.Gray, "Scane 2 - Press 'z' to see Scene 1")
+
+	// Add the quit text to both scenes
+	s.Add(quit)
+	s2.Add(quit)
+
+	// g.Init takes a slice of IScenes
+	ss := []t.IScene{s, s2}
+
+	// Init the Game
+	g.Init(ss)
+
+	// Start the Game
+	g.Start()
+
+}
